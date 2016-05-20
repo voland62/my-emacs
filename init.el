58f06a88996871cf632d5d372271088b124f99c3
Andrew <voland62@gmail.com>
2016-05-20 15:55:02 +0300

    neotree-toggle bind to [f8]

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;;; Commentary: ---  Well for now I write a switch which will be load the presets (curently live or prelude)
 ;;; by command line args. If no supplyed - loads vanilla Emacs.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (set-face-attribute 'default (selected-frame) :height 120)
 
 (global-set-key [(control tab)] 'other-window)
@@ -35,8 +42,18 @@
              (cons "--prelude"
                    (lambda (switch)
                      (load-file "~/reps/prelude/init.el")
+                     (global-set-key [f8] 'neotree-toggle)
 		     ;; (load-file "~/.emacs.d/haskell-mode-setup.el")
 		   )))
+
+(add-to-list 'command-switch-alist
+             (cons "--spacemacs"
+                   (lambda (switch)
+		     (add-to-list
+		      'package-archives
+		      '("melpa" . "http://melpa.org/packages/") t)
+		     (setq user-emacs-directory "~/reps/spacemacs/")
+                     (load-file "~/reps/spacemacs/init.el"))))
 
 (add-to-list 'command-switch-alist
              (cons "--haskell"
@@ -55,3 +72,17 @@
                      (setenv "EMACS_LIVE_DIR" "~/reps/emacs-live/")
                      (load-file "~/reps/emacs-live/init.el")
 		     )))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit-popup crux which-key zop-to-char zenburn-theme yaml-mode web-mode volatile-highlights undo-tree swift-mode sublimity smex smartrep smartparens smart-mode-line scss-mode rainbow-mode rainbow-delimiters projectile ov operate-on-number neotree move-text markdown-mode+ magit json-mode js2-mode imenu-anywhere ido-ubiquitous hyde haskell-mode guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck flx-ido expand-region elm-mode elisp-slime-nav easy-kill discover-my-major diminish diff-hl company cider browse-kill-ring beacon anzu ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
